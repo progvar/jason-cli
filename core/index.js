0c4c25e942aa8cc7d1e381f1f4961e4d9f50b3a2
Gyula Varadi <gyula.varadi@lhsystems.com>
2017-11-28 16:25:05 +0100

    add the core entrypoint

---
+++ core/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const fs = require('fs');
+const cli = require('commander');
+
+cli.version('1.00')
+    .command('copy-schema <source> <target>')
+    .description('-  Apply the schema of the source JSON to the target JSON. Please use relative filepaths!')
+    .action(copySchema)
+
+cli.parse(process.argv)
+
+function copySchema(source, target) {
+    console.log(source, target);
+}
+
